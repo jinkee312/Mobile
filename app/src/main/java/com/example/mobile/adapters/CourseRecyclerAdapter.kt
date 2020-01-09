92dee457ebebf38c2f5dbd556ef3a5695f914b16
ASUS <koayjk-pm17@student.tarc.edu.my>
2020-01-09 19:15:38 +0800

    commit

--- app/src/main/java/com/example/mobile/adapters/CourseRecyclerAdapter.kt
+++ app/src/main/java/com/example/mobile/adapters/CourseRecyclerAdapter.kt
@@ -2,25 +2,17 @@ package com.example.mobile.adapters
 
 
 import android.app.AlertDialog
-import android.app.PendingIntent
 import android.content.Context
 import android.content.DialogInterface
-import android.text.TextUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.INVISIBLE
 import android.view.ViewGroup
 import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.layout_blog_list_item.view.*
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import com.example.mobile.R
-import com.example.mobile.models.BlogPost
 import com.example.mobile.models.Course
 import com.google.firebase.database.DatabaseReference
-import kotlin.collections.ArrayList
 import com.google.firebase.database.FirebaseDatabase
 
 
@@ -37,7 +29,7 @@ class BlogRecyclerAdapter (val courseList: List<Course>, val context: Context): 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BlogViewHolder {
         return BlogViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.layout_blog_list_item, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.layout_course_list_item, parent, false)
         )
     }
 
